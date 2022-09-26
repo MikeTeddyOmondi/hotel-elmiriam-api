3afd16fef5a30103309b7310fe223a07beca55c0
MikeTeddyOmondi <mike_omondi@outlook.com>
2022-09-26 13:33:32 +0300

    Updated debug value to us NODE_ENV

--- api/hotel/src/config/config.js
+++ api/hotel/src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./src/.env", debug: true });
+require("dotenv").config({ path: "./src/.env", debug: process.env.NODE_ENV === "development" ? true : false });
 
 const { REFRESH_SECRET, ACCESS_SECRET, DB_URL, PORT } = process.env;
 
