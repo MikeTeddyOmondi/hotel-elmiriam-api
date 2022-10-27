8bb7654849b33338f9575479dedde72f4b2d4589
MikeTeddyOmondi <mike_omondi@outlook.com>
2022-10-28 02:01:49 +0300

    Updates hotel api | Documented routes information

--- api/hotel/src/routes/routes.js
+++ api/hotel/src/routes/routes.js
@@ -13,22 +13,49 @@ const {
 	createRoomType,
 	fetchOneRoom,
 	allRoomTypes,
-	fetchAllRooms
+	fetchAllRooms,
 } = require("../controllers/");
 
+// READ API Information
 router.get("/", ApiInfo);
+
+// READ API Information
 router.get("/api/v1", ApiInfo);
+
+// READ ALL CUSTOMERS
 router.get("/api/v1/customers", getAllCustomers);
+
+// CREATE NEW CUSTOMER
 router.post("/api/v1/customers", addCustomer);
+
+// READ CUSTOMER | Search - Reuqires: idnumber
 router.get("/api/v1/customers/:idnumber", searchCustomer);
+
+// READ ALL BOOKINGS
 router.get("/api/v1/bookings", getAllBookings);
+
+// CREATE NEW BOOKING
 router.post("/api/v1/bookings", addBookings);
+
+// READ ALL INVOICES
 // router.get("/api/v1/invoices/", getAllBookingInvoice);
+
+// READ ONE INVOICE
 // router.get("/api/v1/invoices/:id", getBookingInvoice);
+
+// READ ALL ROOMS
 router.get("/api/v1/rooms", fetchAllRooms);
+
+// READ ALL ROOM TYPES AVAILABLE
 router.get("/api/v1/rooms/types", allRoomTypes);
+
+// CREATE NEW ROOM TYPE
 router.post("/api/v1/rooms/types", createRoomType);
+
+// CREATE ROOM | Requires: roomtypeid
 router.post("/api/v1/rooms/:roomtypeid", createRoom);
+
+// READ ROOM INFO | Requires: roomid
 router.get("/api/v1/rooms/:roomid", fetchOneRoom);
 
 module.exports = router;
