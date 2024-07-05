cd2808a5a21f6c5ed8caf0b4564bfb52ca882dea
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-07-05 17:52:57 +0300

    Updated: refactored bar service - Drink Model

--- api/bar/src/models/Drink.js
+++ api/bar/src/models/Drink.js
@@ -18,14 +18,38 @@ const DrinkSchema = new mongoose.Schema(
     },
     uom: {
       type: String,
-      enum: ["bottles", "crates"],
+      enum: ["bottles", "crates", "pack"],
+      required: true,
+    },
+    packageQty: {
+      type: Number,
       required: true,
     },
     buyingPrice: {
       type: Number,
+      required: false,
+      default: 0,
+      // default: function () {
+      //   return this.stockQty !== 0
+      //     ? Number(this.buyingStockPrice) / Number(this.packageQty)
+      //     : 0;
+      // },
+    },
+    buyingStockPrice: {
+      type: Number,
       required: true,
     },
     sellingPrice: {
+      type: Number,
+      required: false,
+      default: 0,
+      // default: function () {
+      //   return this.stockQty !== 0
+      //     ? Number(this.sellingStockPrice) / Number(this.packageQty)
+      //     : 0;
+      // },
+    },
+    sellingStockPrice: {
       type: Number,
       required: true,
     },
@@ -44,6 +68,27 @@ const DrinkSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// function updateBuyingPrice() {
+//   return this.stockQty !== 0
+//     ? (this.buyingStockPrice =
+//         Number(this.buyingStockPrice) / Number(this.packageQty))
+//     : 0;
+// }
+
+// function updateSellingPrice() {
+//   return this.stockQty !== 0
+//     ? (this.sellingStockPrice =
+//         Number(this.sellingStockPrice) / Number(this.packageQty))
+//     : 0;
+// }
+
+// DrinkSchema.pre("save", function (next) {
+//   // Logic
+//   updateBuyingPrice();
+//   updateSellingPrice();
+//   next();
+// });
 
 const Drink = mongoose.model("Drink", DrinkSchema);
 
